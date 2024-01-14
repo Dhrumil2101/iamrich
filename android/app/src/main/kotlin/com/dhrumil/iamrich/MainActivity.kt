f7e5a8484d0feaa681c739450a2dd00f63f36092
Dhrumil <dhrumil@dhrumils-mbp.home>
2024-01-14 12:42:16 -0500

    simple layout  of iamrich

---
+++ android/app/src/main/kotlin/com/dhrumil/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dhrumil.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text("I am rich"),
+        backgroundColor: Colors.blue[700],
+      ),
+    ),
+    ),
+  );
+}
